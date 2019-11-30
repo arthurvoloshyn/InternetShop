0b04b8058c72115338086997e8113f5603654a86
Artur <grandfaraon93@gmail.com>
2019-11-30 19:56:13 +0200

    Updates

---
+++ precache-manifest.ac08347885602cab6ceba56aa6636baa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c881c4b7670309a41555d851e3f401",
+    "url": "/InternetShop/index.html"
+  },
+  {
+    "revision": "8941e74c032d338473cb",
+    "url": "/InternetShop/static/css/main.8bd842fe.chunk.css"
+  },
+  {
+    "revision": "03a7a2566c2b631c5dea",
+    "url": "/InternetShop/static/js/2.8dc9ef82.chunk.js"
+  },
+  {
+    "revision": "8941e74c032d338473cb",
+    "url": "/InternetShop/static/js/main.68171ac4.chunk.js"
+  },
+  {
+    "revision": "9abdeb153c0194bae2a4",
+    "url": "/InternetShop/static/js/runtime~main.21f753c0.js"
+  }
+]);
