f0f3b28b6e864ff6bd642f50a421eb3365b8451a
Artur <grandfaraon93@gmail.com>
2019-12-03 16:43:17 +0200

    Router update

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 
 import App from './components/app';
 import ErrorBoundry from './components/error-boundry';
@@ -16,7 +16,7 @@ ReactDOM.render(
   <Provider store={store}>
     <ErrorBoundry>
       <BookstoreServiceProvider value={bookstoreService}>
-        <Router basename='/InternetShop/'>
+        <Router>
           <App />
         </Router>
       </BookstoreServiceProvider>
