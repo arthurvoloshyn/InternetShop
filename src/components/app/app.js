cf87da85bff8aa6466e5357774a02b86baf25593
Artur <grandfaraon93@gmail.com>
2020-01-13 19:48:42 +0200

    App updates

---
+++ src/components/app/app.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Route, Switch, HashRouter as Router } from 'react-router-dom';
+
+import BookstoreService from '../../services/bookstore-service';
+
+import { BookstoreServiceProvider } from '../../context/bookstore-service-context';
+
+import store from '../../store/';
+
+import ShopHeader from '../../containers/shop-header';
+
+import ErrorBoundry from '../error-boundry';
+import { HomePage, CartPage } from '../pages';
+
+const bookstoreService = new BookstoreService();
+
+const App = () => (
+  <Provider store={store}>
+    <ErrorBoundry>
+      <BookstoreServiceProvider value={bookstoreService}>
+        <Router>
+          <main role="main" className="container">
+            <ShopHeader />
+
+            <Switch>
+              <Route path="/" component={HomePage} exact />
+
+              <Route path="/cart" component={CartPage} />
+            </Switch>
+          </main>
+        </Router>
+      </BookstoreServiceProvider>
+    </ErrorBoundry>
+  </Provider>
+);
+
+export default App;
