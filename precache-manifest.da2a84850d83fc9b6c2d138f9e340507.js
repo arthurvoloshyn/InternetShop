5a1ba7103ec8f43d723f0e92154aacdd95623e02
Artur <grandfaraon93@gmail.com>
2020-01-28 18:08:05 +0200

    Updates

--- precache-manifest.da2a84850d83fc9b6c2d138f9e340507.js
+++ precache-manifest.da2a84850d83fc9b6c2d138f9e340507.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b783f66c3099c1fa258dd0446db7ca39",
+    "revision": "6c8f26deb46946eca7f5600fe9ce836d",
     "url": "/InternetShop/index.html"
   },
   {
-    "revision": "9270616cb7e836096fda",
-    "url": "/InternetShop/static/css/main.dde0a9d3.chunk.css"
+    "revision": "6567378822c19a0e2fee",
+    "url": "/InternetShop/static/css/main.052d6dd0.chunk.css"
   },
   {
     "revision": "2193a728214e576c6cfc",
     "url": "/InternetShop/static/js/2.cd07d21b.chunk.js"
   },
   {
-    "revision": "9270616cb7e836096fda",
-    "url": "/InternetShop/static/js/main.8113b446.chunk.js"
+    "revision": "6567378822c19a0e2fee",
+    "url": "/InternetShop/static/js/main.6e3959e0.chunk.js"
   },
   {
     "revision": "9abdeb153c0194bae2a4",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/InternetShop/precache-manifest.3f7b05820364e32532a0911814c73fd0.js"
+  "/InternetShop/precache-manifest.da2a84850d83fc9b6c2d138f9e340507.js"
 );
 
 self.addEventListener('message', (event) => {
